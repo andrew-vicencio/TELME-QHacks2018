46e02f2aa76e9bdd9872602aa1bb368ab4040a60
rykrr <hiroshikerr@gmail.com>
2018-02-03 23:12:59 -0500

    Audio

--- hw/electron.ino
+++ hw/electron.ino
@@ -1,45 +1,84 @@
+/* QHacks 2018
+ *
+ * Audio Reference:
+ * from https://github.com/rickkas7/photonAudioSample1/
+ */
+ 
 #include "Particle.h"
 #include "cellular_hal.h"
+#include "spk.h"
 
 STARTUP(cellular_credentials_set("isp.telus.com", "", "", NULL));
+
+// Audio Buffer Constants
+#define AB_SIZE 512
+#define AB_BUFS 16
+
+#define SR 8000
+
+IntervalTimer T;
+
+byte rec();
+void blink();
+void lvl(int);
 
 TCPClient cc;
 byte srv[] = { 138, 197, 152, 152 };
 int pt = 2000;
 
-void blink();
-void lvl(int);
+uint16_t ab[AB_BUFS][AB_SIZE];
+uint16_t x, y;
+
+unsigned long ref;
+
+byte state = 0;
 
 void setup() {
     Serial.begin(9600);
-    
-    Wire.begin(0x05);
-    Wire.onRequest(blink);
     
     pinMode(A0, INPUT);
     pinMode(B0, OUTPUT);
     pinMode(B1, OUTPUT);
     pinMode(B2, OUTPUT);
+    pinMode(B3, OUTPUT);
+    pinMode(B4, OUTPUT);
 	pinMode(D7, OUTPUT);
 }
 
 void loop() {
-    lvl(9000);
-    delay(1000);
-    lvl(0);
-    delay(1000);
+    delay(4);
+    lvl(analogRead(A0));
+    
+    switch(state) {
+        case 0:
+            for(int i=0; i<AB_BUFS; i++)
+                for(int j=0; j<AB_SIZE; j++)
+                    ab[i][j] = 0;
+            break;
+        case 1:
+            T.begin(rec, 1000000 / SP, uSec);
+            ref = millis();
+            break;
+        case 2:
+            if(x < AB_SIZE) {
+                
+            }
+        case 3:
+    }
+}
+
+byte rec() {
+    return (uint8_t) analogRead(A0);
 }
 
 /*
 void loop () {
-    delay(4);
-    lvl(analogRead(A0));
     int e = 0;
     for(int i=1; i<127; i++) {
         Wire.beginTransmission(i);
         e = Wire.endTransmission();
         
-        if(!e) {
+        f(!e) {
             Serial.print("Detected @ ");
             Serial.println(i);
         }
@@ -69,13 +108,17 @@ void loop () {
 void lvl(int l) {
     int hl = 0;
     Serial.println(l);
-    if(l > 750) hl |= 1;
-    if(l > 900) hl |= 2;
-    if(l > 1150) hl |= 4;
+    if(l > 500) hl |= 1;
+    if(l > 600) hl |= 2;
+    if(l > 700) hl |= 4;
+    if(l > 800) hl |= 8;
+    if(l > 900) hl |= 16;
     
     digitalWrite(B0, hl&1 ? HIGH : LOW);
     digitalWrite(B1, hl&2 ? HIGH : LOW);
     digitalWrite(B2, hl&4 ? HIGH : LOW);
+    digitalWrite(B3, hl&8 ? HIGH : LOW);
+    digitalWrite(B4, hl&16 ? HIGH : LOW);
 }
 
 void blink() {
