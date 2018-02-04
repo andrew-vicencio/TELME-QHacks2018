878276e95cdfa480cff0d810c748a7a24bfa1968
bKolisnik <bKolisnik@gmail.com>
2018-02-03 21:01:09 -0500

    First push to heroku

--- app/server_app.py
+++ app/server_app.py
@@ -1,4 +1,23 @@
 from aiohttp import web
+import socketio
+import pyaudio
+#import websocket
+
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+#wsURI = 'wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize' + '?watson-token'
+#session = aiohttp.ClientSession.ws_connect(wsURI)
+
+
+
+sio = socketio.AsyncServer(async_mode='aiohttp')
+app = web.Application()
+sio.attach(app)
+
 
 async def handle(request):
     return web.Response(text="Hello World")
@@ -6,7 +25,17 @@ async def handle(request):
 
 
 
-app = web.Application()
+#microcontroller emits event called dataFromMicro
+#@sio.on('dataFomMicro')
+#async def getDataFromMicro(request):
+
+
+
+
+@sio.on('data')
+def print_data(sid, message):
+    print(message)
+
 app.router.add_get('/',handle)
 
 web.run_app(app, host='127.0.0.1', port=8080)
