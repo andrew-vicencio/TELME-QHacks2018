da343f85e1dfa6527e3a6a55e512a3063ae400f6
andrew-vicencio <andrew.v1124@gmail.com>
2018-02-24 00:21:07 -0500

    Edited items to .gitignore. Removed test scripts. Fixed ajax requests.

--- app/website/Scripts/scripts.js
+++ app/website/Scripts/scripts.js
@@ -2,10 +2,22 @@ var myAPI = "8B18mZhs2uqttEfDJ95xx7PTk8bohh7n";
 var serverURL = "http://localhost:8080/";
 var mlabURL = "https://api.mlab.com/api/1";
 var getURL = "/databases/convodb/collections";
-var textURL = "/getDataText";
-var AnalysisURL = "/getDataAnalysis";
+var textURL = "getDataText";
+var AnalysisURL = "getDataAnalysis";
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', serverURL+textURL, true);
-xhr.send();
+var xhr = $.get({
+  url: serverURL + AnalysisURL,
+  success: function(){
+    console.log("It worked!");
+  }
+});
+
+console.log("Status:");
+console.log(xhr.status);
+console.log("Status Text:");
+console.log(xhr.statusText);
+console.log("Response Header:");
 console.log(xhr.getAllResponseHeaders());
+console.log("Response Text:");
+console.log(xhr.responseText);
+console.log(xhr);
